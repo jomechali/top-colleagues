de156ccc5199066bb1a118b7b806ff5c6d8fee4e
Joseph MECHALI <mechali.joseph@gmail.com>
2022-08-24 16:28:39 +0200

    component colleague

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ColleagueComponent } from './colleague/colleague.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColleagueComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/colleague/colleague.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tc-colleague',
+  templateUrl: './colleague.component.html',
+  styleUrls: ['./colleague.component.scss']
+})
+export class ColleagueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
